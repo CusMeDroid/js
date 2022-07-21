3b02605b44085fcf0452f738b545d787b7f93bbd
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2022-07-21 16:02:50 +0700

    Update cv-jQuery.js

--- cv-jQuery.js
+++ cv-jQuery.js
@@ -1,26 +1,3 @@
-function submitFunc() {
-    var name = document.getElementById('name');
-    var phone = document.getElementById('phone');
-    var subject = document.getElementById('subject');
-    if (name.value < 1 || phone.value < 1 || subject.value < 1) {
-        alert('Please fill out this field!');
-    } else {
-        window.open('mailto:iyortml@gmail.com?subject='+name.value+', '+phone.value+'&body='+subject.value);
-    }
-}
-
-if ('mediaSession' in navigator) {
-    navigator.mediaSession.metadata = new MediaMetadata({
-        artist : 'THREESIXTY',
-        title: 'JAWABAN DIBALIK SENYUMAN',
-        artwork: [{
-            src: 'https://i.ytimg.com/vi/p6G2ajnqB5c/maxresdefault.jpg',
-            sizes: '320x180',
-            type: 'image/png'
-        }]
-    })
-}
-
 var meaudio = document.getElementById('myAudio'); 
 var mp1 = document.getElementById('musicpl'); 
 var mp2 = document.getElementById('musicpa'); 
