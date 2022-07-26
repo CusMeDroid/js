4e5e6131ceddcbad0107bb00b7b6e43bdac673c7
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2022-07-26 10:32:11 +0700

    Update cv-jQuery.js

--- cv-jQuery.js
+++ cv-jQuery.js
@@ -31,46 +31,6 @@ function pauseAudio() {
 }
 */
 
-/* Embed play/pause */
-var player;
-
-function onYouTubePlayerAPIReady() {
-    player = new YT.Player('music-video', {
-        events: {
-            // call this function when player is ready to use
-            'onReady': onPlayerReady
-        }
-    });
-}
-
-function onPlayerReady(event) {
-    var playButton = document.getElementById("musicpl");
-    var pauseButton = document.getElementById("musicpa");
-    var playButton2 = document.getElementById("musicp2");
-    var pauseButton2 = document.getElementById("musicpa2");
-    playButton.addEventListener("click", function() {
-        player.playVideo();
-    	playButton.style.display = "none";
-    	pauseButton.style.display = "block";
-    	playButton2.style.display = "none";
-    	pauseButton2.style.display = "block";
-    });
-    pauseButton.addEventListener("click", function() {
-        player.pauseVideo();
-    	playButton.style.display = "block";
-    	pauseButton.style.display = "none";
-    	playButton2.style.display = "block";
-    	pauseButton2.style.display = "none";
-    });
-
-}
-
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-/* End embed play/pause */
-
 window.onscroll = function() {
   scrollFunction()
 };
