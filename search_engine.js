3fa8e1ca3366210cf8528ee5cc0a37b468f304c5
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2022-07-31 18:52:41 +0700

    Create search_engine.js

---
+++ search_engine.js
@@ -0,0 +1,34 @@
+/* Fungtion for autocomplete
+* Add your sugest in here
+* This is project by CusMeDroid
+* IyoRTML
+* Suryo DwiJayanto
+*/
+var repositories = ['guide/html','guide/css','guide/js','guide/audio','guide/video',
+  'home','docs','roadmap','privacy',
+  'cv','cv/suryodwijayanto','cv/sahrul','cv/mohjakayulianto','Suryo DwiJayanto','sahrul','Moh Jaka Yulianto',
+  'android-studio','ip2location-contest','j-shop','blog','desktop'
+];
+autocomplete(document.getElementById('search'), repositories);
+/* Function button click
+* Last your input sugest you can add action here!
+* This is project by CusMeDroid
+* IyoRTML
+* Suryo DwiJayanto
+*/
+function submitFunc() {
+    var search = document.getElementById('search');
+    if (search.value < 1) {
+        alert('Please fill out this field!');
+    } else if(search.value > 1) {
+        if (search.value == 'Suryo DwiJayanto') {
+            window.location.href = 'cv/suryodwijayanto';
+        } else if (search.value == 'Sahrul') {
+            window.location.href = 'cv/sahrul';
+        } else if (search.value == 'cv/mohjakayulianto') {
+            window.location.href = search.value;
+        } else {
+            window.location.href = search.value;
+        }
+    }
+}
