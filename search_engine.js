a49d8e2e080e9f5fe6294af02c4207423c47b417
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2022-07-31 19:21:46 +0700

    Update search_engine.js

--- search_engine.js
+++ search_engine.js
@@ -5,7 +5,7 @@
 * Suryo DwiJayanto
 */
 var repositories = ['guide/html','guide/css','guide/js','guide/audio','guide/video',
-  'home','docs','roadmap','privacy','IyoRTML',
+  'home','docs','roadmap','privacy','IyoRTML','Amazon','Google','Yahoo','Twitter','Facebook','Instagram','TikTok',
   'cv','cv/suryodwijayanto','cv/sahrul','cv/mohjakayulianto','Suryo DwiJayanto','Sahrul','Moh Jaka Yulianto',
   'android-studio','ip2location-contest','j-shop','blog','desktop'
 ];
@@ -28,6 +28,22 @@ function submitFunc() {
         window.location.href = 'cv/mohjakayulianto';
     } else if (search.value == 'IyoRTML') {
         window.location.href = 'https://google.com/search?q=IyoRTML';
+    }
+    /* Medsos */
+    else if (search.value == 'Amazon') {
+        window.location.href = 'https://amazon.com';
+    } else if (search.value == 'Google') {
+        window.location.href = 'https://google.com/search?q=CusMeDroid';
+    } else if (search.value == 'Yahoo') {
+        window.location.href = 'https://yahoo.com/search?q=CusMeDroid';
+    } else if (search.value == 'Twitter') {
+        window.location.href = 'https://twitter.com/IyoRTML';
+    } else if (search.value == 'Facebook') {
+        window.location.href = 'https://facebook.com/suryo.dwijayantoskom';
+    } else if (search.value == 'Instagram') {
+        window.location.href = 'https://instagram.com/suryodwijayanto';
+    } else if (search.value == 'TikTok') {
+        window.location.href = 'https://tiktok.com/@suryodwijayanto';
     } else {
         window.location.href = search.value;
     }
