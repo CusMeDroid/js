1f79a565a5ed4d82a25418cfb0487ed8cf6b5d2e
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2022-08-05 01:17:01 +0700

    Update cv-jQuery.js

--- cv-jQuery.js
+++ cv-jQuery.js
@@ -99,3 +99,18 @@ $(document).ready(function(){
     $("#cmenu").slideToggle('fast');
   });
 });
+
+var today = new Date();
+var year = 0;
+if (today.getMonth() < birthday.getMonth()) {
+    year = 1;
+} else if ((today.getMonth() == birthday.getMonth()) && today.getDate() < birthday.getDate()) {
+    year = 1;
+}
+var age = today.getFullYear() - birthday.getFullYear() - year;
+
+if(age < 0){
+    age = 0;
+}
+document.getElementById('myold').innerHTML = age;
+console.log(today +' + '+ birthday + ' = ' + age);
