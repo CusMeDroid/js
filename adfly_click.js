82640bd05f3264ef10b48edf1006b0d1d4fdceb0
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2022-08-09 23:32:57 +0700

    Create adfly_click.js

---
+++ adfly_click.js
@@ -0,0 +1,9 @@
+function myAds1() {
+    window.open('http://j.gs/2428724/ads01');
+}
+function myAds2() {
+    window.open('http://j.gs/2428724/ads02');
+}
+function myAds3() {
+    window.open('http://j.gs/2428724/ads03');
+}
