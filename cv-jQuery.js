7614dd8401ed9a568c673a747403d3151327a773
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2022-08-09 22:49:27 +0700

    Update cv-jQuery.js

--- cv-jQuery.js
+++ cv-jQuery.js
@@ -78,16 +78,6 @@ function scrollFunction() {
   }
 }
 
-/* Me Menu Main */
-function myFunction(memenu) {
-  memenu.classList.toggle("change");
-}
-$(document).ready(function(){
-  $("#smenu").click(function(){
-    $("#cmenu").slideToggle('fast');
-  });
-});
-
 /* onclick */
 function meHome() {
     window.location.href = '#';
@@ -109,29 +99,3 @@ function mStudy() {
 function mContact() {
     window.location.href = '#contact';
 }
-
-/* Navbar bottom new */
-var menu_bar = document.querySelector('.me-bottom-bar');
-var menu_item = document.querySelectorAll('.me-menu-item');
-var menu_indicator = document.querySelector('.me-nav-indicator');
-var menu_current_item = document.querySelector('.me-current');
-var menu_position;
-
-menu_position = menu_current_item.offsetLeft - 16;
-menu_indicator.style.left = menu_position + 'px';
-menu_bar.style.backgroundPosition = menu_position -8 + 'px';
-menu_item.forEach(
-  function(select_menu_item){
-    select_menu_item.addEventListener('click', function(e){
-      e.preventDefault();
-      menu_position = this.offsetLeft - 16;
-      menu_indicator.style.left = menu_position + 'px';
-      menu_bar.style.backgroundPosition = menu_position -8 + 'px';
-      [...select_menu_item.parentElement.children].forEach(
-        sibling => {
-          sibling.classList.remove('me-current');
-        })
-      select_menu_item.classList.add('me-current');
-    });
-  }
-)
