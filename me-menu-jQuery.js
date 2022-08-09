471bd48f42274aca1d6518f6c09bd78d1d27d955
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2022-08-09 22:50:48 +0700

    Create me-menu-jQuery.js

---
+++ me-menu-jQuery.js
@@ -0,0 +1,9 @@
+/* Me Menu Main */
+function myFunction(memenu) {
+  memenu.classList.toggle("change");
+}
+$(document).ready(function(){
+  $("#smenu").click(function(){
+    $("#cmenu").slideToggle('fast');
+  });
+});
