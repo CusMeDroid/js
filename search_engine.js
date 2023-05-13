ff277a225852d557f0655e1043c29731fa0adb41
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2023-05-13 09:09:51 +0700

    Update search_engine.js

--- search_engine.js
+++ search_engine.js
@@ -24,7 +24,7 @@ function submitFunc() {
     } else if (search.value == 'Live Code Editor') {
         window.location.href = 'live-code-editor';
     } else if (search.value == 'Scan Barcode') {
-        window.location.href = 'live-code-editor/camera';
+        window.location.href = 'live-making-barcode/camera';
     } else if (search.value == 'Live Making Barcode') {
         window.location.href = 'live-making-barcode';
     } else if (search.value == 'TV Streaming Indonesia') {
